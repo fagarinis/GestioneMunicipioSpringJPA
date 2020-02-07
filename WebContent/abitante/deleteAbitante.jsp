1f9d761760cb0b029247ebcea76dadff92987a89
Federico Agarinis <federicof1994@gmail.com>
2020-02-07 17:18:58 +0100

    result.jsp e cancella di abitante

--- WebContent/abitante/deleteAbitante.jsp
+++ WebContent/abitante/deleteAbitante.jsp
@@ -1,12 +1,59 @@
+<%@page import="it.prova.gestionemunicipiospringjpa.model.Abitante"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html>
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Cancella Abitante</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 </head>
 <body>
+
+	<%		//test
+			//Abitante abitanteInPagina = new Abitante("mario", "rossi", 25, "via mosca");
+		Abitante abitanteInPagina = (Abitante) request.getAttribute("abitanteInput");
+	%>
+
+	<div class="container">
+	
+		
+		<%@ include file="/header.jsp"%> 
+
+		<div class="page-header">
+			<h3>Pagina di conferma cancellazione</h3>
+		</div>
+		<div class="container-fluid">
+			<dl class="row">
+				<dt class="col-sm-3 text-right">Id</dt>
+				<dd class="col-sm-9"><%=abitanteInPagina.getId()%></dd>
+			</dl>
+			<dl class="row">
+				<dt class="col-sm-3 text-right">Nome</dt>
+				<dd class="col-sm-9"><%=abitanteInPagina.getNome()%></dd>
+			</dl>
+			<dl class="row">
+				<dt class="col-sm-3 text-right">Cognome</dt>
+				<dd class="col-sm-9"><%=abitanteInPagina.getCognome()%></dd>
+			</dl>
+			<dl class="row">
+				<dt class="col-sm-3 text-right">Eta</dt>
+				<dd class="col-sm-9"><%=abitanteInPagina.getEta() %></dd>
+			</dl>
+			<dl class="row">
+				<dt class="col-sm-3 text-right">Residenza</dt>
+				<dd class="col-sm-9"><%=abitanteInPagina.getResidenza() %></dd>
+			</dl>
+
+		</div>
+		
+		
+		<form action = "EseguiEliminaPersonaServlet" method ="post">
+			<input type = "hidden" name = "idAbitante" value = "<%=abitanteInPagina.getId()%>" >
+			<input type ="submit" value ="Cancella">
+		</form>
+
+	</div>
 
 </body>
 </html>
--- src/it/prova/gestionemunicipiospringjpa/web/servlet/abitante/ExecuteDeleteAbitanteServlet.java
+++ src/it/prova/gestionemunicipiospringjpa/web/servlet/abitante/ExecuteDeleteAbitanteServlet.java
@@ -1,11 +1,21 @@
 package it.prova.gestionemunicipiospringjpa.web.servlet.abitante;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import it.prova.gestionemunicipiospringjpa.model.Abitante;
+import it.prova.gestionemunicipiospringjpa.service.abitante.AbitanteService;
 
 /**
  * Servlet implementation class ExecuteDeleteAbitanteServlet
@@ -13,29 +23,57 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ExecuteDeleteAbitanteServlet")
 public class ExecuteDeleteAbitanteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExecuteDeleteAbitanteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
+	@Autowired
+	private AbitanteService abitanteService;
+
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	}
+	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ExecuteDeleteAbitanteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//#test
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		Long idAbitanteDaCancellare = Long.parseLong(request.getParameter("idAbitante"));
+		Abitante abitanteDaCancellare = new Abitante();
+		abitanteDaCancellare.setId(idAbitanteDaCancellare);
+
+		//esecuzione rimuovi
+		abitanteService.rimuovi(abitanteDaCancellare);
+		
+		//ricreo la lista di ricerca
+		List<Abitante> listaAbitanti = abitanteService.listAllAbitanti();
+		request.setAttribute("listaAbitantiAttributeName", listaAbitanti);
+		
+		
+		RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+		rd.forward(request, response);
 	}
 
 }
